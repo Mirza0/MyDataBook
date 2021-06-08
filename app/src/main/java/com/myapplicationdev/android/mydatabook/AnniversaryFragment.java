feac46756f975ca49f9c866fd1a5195e5eaee769
19002765 <19002765@myrp.edu.sg>
2021-06-08 13:00:42 +0800

    fghjkl

--- app/src/main/java/com/myapplicationdev/android/mydatabook/AnniversaryFragment.java
+++ app/src/main/java/com/myapplicationdev/android/mydatabook/AnniversaryFragment.java
@@ -1,10 +1,14 @@
 package com.myapplicationdev.android.mydatabook;
 
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -15,9 +19,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class AnniversaryFragment extends Fragment {
     Button btnEditAnn;
     TextView tvAnn;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle toggle;
+    FloatingActionButton btnAnnSearch;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,6 +34,7 @@ public class AnniversaryFragment extends Fragment {
 
     }
 
+    @SuppressLint("ResourceAsColor")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -32,6 +42,7 @@ public class AnniversaryFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_anniversary, container, false);
         btnEditAnn = v.findViewById(R.id.btnAnn);
         tvAnn = v.findViewById(R.id.tvAnn);
+        btnAnnSearch = v.findViewById(R.id.fab);
 
         btnEditAnn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,6 +69,17 @@ public class AnniversaryFragment extends Fragment {
 
             }
         });
+
+         btnAnnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                View main_view = inflater.inflate(R.layout.activity_main, null);
+                drawerLayout = main_view.findViewById(R.id.drawer_layout);
+                toggle = new ActionBarDrawerToggle(getActivity(), drawerLayout, R.string.drawer_open, R.string.drawer_close);
+                drawerLayout.addDrawerListener(toggle);
+            }
+        });
+
         return v;
     }
 }
